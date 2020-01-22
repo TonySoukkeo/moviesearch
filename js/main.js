751714c2880ad9e0dca9de41186c25b3d0b1f25c
Tony <tonysoukkeodesign@gmail.com>
2020-01-22 00:18:13 -0700

    CSS updated for larger screens

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const URL = "http://www.omdbapi.com/";
 
 $(document).ready(() => {
-  // $(".search-input").on("keyup", searchMovie);
+  $(".search-input").on("keyup", searchMovie);
 });
 
 // Function to search for movie
@@ -17,9 +17,27 @@ function searchMovie(e) {
     } else {
       // Loop through search results and create list items for each
       $.each(data.Search, (index, item) => {
-        output += `<li class="display__card">
-                      <h3 class="display__card--title">${item.Title}</h3>
-                   </li>`;
+        output += `        <li class="display__card">
+        <div class="display__card-poster">
+          <img
+            class="display__card-poster--img"
+            src="${item.Poster}"
+            alt="banner"
+          />
+        </div>
+
+        <div class="display__card--body">
+          <h3 class="display__card--title">${item.Title}</h3>
+
+          <p><b>Release Date:</b> <em>${item.Year}</em></p>
+          <br />
+          <p><b>Type:</b> <em>${item.Type}</em></p>
+        </div>
+
+        <div class="display__card--btn">
+          <p>Info</p>
+        </div>
+      </li>`;
       });
     }
 
